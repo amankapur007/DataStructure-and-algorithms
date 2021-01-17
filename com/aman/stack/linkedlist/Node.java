1c4050116056b28403c2d697fb97957604c08377
Aman Kapur <akapur07@gmail.com>
2021-01-18 02:29:55 +0530

    Stack using linked list

---
+++ com/aman/stack/linkedlist/Node.java
@@ -0,0 +1,34 @@
+package com.aman.stack.linkedlist;
+
+public class Node {
+
+    private int data;
+    private Node nextNode;
+
+    public Node(int data){
+        this.data = data;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    @Override
+    public String toString() {
+        return data+"";
+    }
+
+    
+}
